1095d2300ea50c205986215e7ac21137da41b604
Akihiro Ito <akihiro@peano-system.jp>
2019-08-22 09:06:55 +0900

    タブをスペースでリプレースする

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 /* ページ内のアンカーへのリンクをクリックした際のスクロールする挙動 */
 /******************************************************************************/
 $(function() {
-	"use strict";
+    "use strict";
     $("a[href^='#']").click(function(){
         var speed = 500;
         var href = $(this).attr("href");
@@ -17,7 +17,7 @@ $(function() {
 /* ページのトップまでスクロールして戻るボタンの挙動 */
 /******************************************************************************/
 (function($) {
-	"use strict";
+    "use strict";
     $.fn.btnScroll = function() {
         var speed = 300;
         return this.each(function() {
@@ -39,38 +39,38 @@ $(function() {
 //グローバルナビ
 //**********************************************************************/
 (function($) {
-	"use strict";
+    "use strict";
     $.fn.globalNav = function() {
         return this.each(function() {
-        	var $window = $(window);
-        	var $this = $(this);
-	        // リサイズ後の遅延実行 xs PC・タブレットで表示
-			var timer = false;
-		    $window.resize(function() {
-		        if(timer !== false) {
-		            clearTimeout(timer);
-		        }
-		        timer = setTimeout(function() {
-				    if ($window.width() > 769) {
-				        $this.find('ul').show();
-				    } else {
-				        $this.find('ul').hide();
-				    }
-				});
-	        });
-		    // スクロール後のナビ位置固定
-		    $window.scroll(function() {
-		        if ($window.scrollTop() > 90) {
+            var $window = $(window);
+            var $this = $(this);
+            // リサイズ後の遅延実行 xs PC・タブレットで表示
+            var timer = false;
+            $window.resize(function() {
+                if(timer !== false) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(function() {
+                    if ($window.width() > 769) {
+                        $this.find('ul').show();
+                    } else {
+                        $this.find('ul').hide();
+                    }
+                });
+            });
+            // スクロール後のナビ位置固定
+            $window.scroll(function() {
+                if ($window.scrollTop() > 90) {
                     $this.addClass('fixed');
-		        } else {
-		            $this.removeClass('fixed');
-		        }
-		    });
-		    // ボタン押下時のトグル処理
-		    $this.find('.btn-nav').on('click', function() {
-				$this.find('ul').toggle(500);
-		    });
-	    });
+                } else {
+                    $this.removeClass('fixed');
+                }
+            });
+            // ボタン押下時のトグル処理
+            $this.find('.btn-nav').on('click', function() {
+                $this.find('ul').toggle(500);
+            });
+        });
     };
 })(jQuery);
 
@@ -78,7 +78,7 @@ $(function() {
 //電話番号 タップでコール
 //**********************************************************************/
 $(function() {
-	"use strict";
+    "use strict";
     var ua = navigator.userAgent;
     if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) {
         $('.tel-link').each(function() {
@@ -92,7 +92,7 @@ $(function() {
 /* 初期化スクリプト */
 /******************************************************************************/
 $(function() {
-	"use strict";
-	$('nav').globalNav({});
+    "use strict";
+    $('nav').globalNav({});
     $('#btn-scroll').btnScroll({});
 });
